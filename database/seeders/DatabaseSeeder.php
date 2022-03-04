d2686b1a2c734b8cc89cbc97e490e3fb3d7cf742
Johanu GANDONOU <johanugandonou@gmail.com>
2022-03-04 15:31:34 +0100

    Correction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        $marks = ['FERRARI','FIAT', 'AUDI','BENTLEY','NISSAN', 'HONDA'];
+        $images = ['car1.jpg','car2.jpg', 'car3.jpg','car4.jpg','car5.jpg', 'car6.jpg'];
+
+        for($i=0; $i<6 ; $i++){
+            DB::table('cars')->insert([
+                'mark' =>$marks[$i],
+                'description' => "This car is a category of car body. It is a medium-sized car, usually between 4 and 5 meters long, with a fixed hardtop roof (unlike a coupé-cabriolet), four doors, four windows and at least four seats on board.",
+                'image_name'=>$images[$i],
+                'image_path'=>"images/".$images[$i],
+                'price_per_day' => 100+($i*10),
+                'plate_number' => "AC148596"
+            ]);
+        }
     }
 }
